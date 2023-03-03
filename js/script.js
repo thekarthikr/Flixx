e87c8842ff5dfa5c8034cf0df3fe48e841bdf2c2
Karthik <karthikwebdev@outlook.com>
2023-03-03 13:25:02 +0530

    Add Tv show Details

--- js/script.js
+++ js/script.js
@@ -155,6 +155,79 @@ const displayMovieDetails = async ()=>{
 
 
 
+// Display Show Details Page
+
+
+const displayTvShowDetails = async ()=>{
+  const showId = window.location.search.split('=')[1];
+   
+  const tvShow = await fetchAPI(`tv/${showId}`);
+     
+      displayBackdrop('show',tvShow.backdrop_path);
+
+      const div = document.createElement('div');
+      div.innerHTML = 
+      `
+      <div class="details-top">
+      <div>
+      ${
+        tvShow.poster_path 
+        ? ` <img
+        src="https://image.tmdb.org/t/p/w500${tvShow.poster_path}"
+        class="card-img-top"
+        alt="${tvShow.name}"
+      />`:  
+      `<img
+      src="../images/no-image.jpg"
+      class="card-img-top"
+      alt="${tvShow.name}"
+    />`
+      }
+      </div>
+      <div>
+        <h2> ${tvShow.name} </h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${(tvShow.vote_average).toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Air Date:${tvShow.first_air_date} </p>
+        <p>
+           ${tvShow.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${
+            tvShow.genres.map( genre => `<li> ${genre.name} </li>`).join('')
+          }
+        </ul>
+        <a href="${tvShow.homepage} " target="_blank" class="btn">Visit Show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+        <li><span class="text-secondary">Number Of Episodes:</span> ${tvShow.number_of_episodes} </li>
+        <li>
+          <span class="text-secondary">Last Episode To Air:</span> ${tvShow.last_episode_to_air.name}
+        </li>
+        <li><span class="text-secondary">Status:</span> ${tvShow.status} </li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">
+      ${
+        tvShow.production_companies.map(company => 
+          `<span> ${company.name}</span>`
+         ).join(', ')
+      }
+      </div>
+    </div>
+      `
+ 
+     
+   document.getElementById('show-details').appendChild(div)
+}
+
+
 
 // Fetch API from TMDB API
 
@@ -238,7 +311,7 @@ const init = ()=>{
             displayMovieDetails();
             break;
         case '/tv-details.html':
-             console.log('Tv shows detail')
+            displayTvShowDetails();
              break;
     }
 
