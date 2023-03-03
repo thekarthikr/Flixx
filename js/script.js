0e4512b6f13e483700f6c00ac698fd6962317cfd
Karthik <karthikwebdev@outlook.com>
2023-03-03 11:48:30 +0530

    Update Movie details to DOM

--- js/script.js
+++ js/script.js
@@ -80,8 +80,78 @@ const displayTvShows = async ()=>{
   })
 }
 
+//  Display Movie Details Page
 
+const displayMovieDetails = async ()=>{
+  const movieId = window.location.search.split('=')[1];
+   
+  const movie = await fetchAPI(`movie/${movieId}`);
+     
+     
+ 
+     const div = document.createElement('div');
+     div.innerHTML = 
+     `
+     <div class="details-top">
+     <div>
+     ${
+      movie.poster_path 
+      ? ` <img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`:  
+    `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+    }
+     </div>
+     <div>
+       <h2>${movie.title} </h2>
+       <p>
+         <i class="fas fa-star text-primary"></i>
+         ${(movie.vote_average).toFixed(1)}/ 10
+       </p>
+       <p class="text-muted">Release Date: ${movie.release_date} </p>
+       <p>
+         ${movie.overview}
+       </p>
+       <h5>Genres</h5>
+       <ul class="list-group">
+          ${ 
+            movie.genres.map((genre)=> `<li> ${genre.name} </li>`).join('')
+          }
+       </ul>
+       <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+     </div>
+   </div>
+   <div class="details-bottom">
+     <h2>Movie Info</h2>
+     <ul>
+       <li><span class="text-secondary">Budget:</span> $${
+        addCommasToNumber(movie.budget) 
 
+       } </li>
+       <li><span class="text-secondary">Revenue:</span> $${
+        addCommasToNumber(movie.revenue)
+       } </li>
+       <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes </li>
+       <li><span class="text-secondary">Status:</span> ${movie.release_date} </li>
+     </ul>
+     <h4>Production Companies</h4>
+     <div class="list-group">
+     ${
+       movie.production_companies.map(company => 
+         `<span> ${company.name}</span>`
+        ).join(', ')
+     }
+     </div>
+   </div>
+     `
+   document.getElementById('movie-details').appendChild(div)
+}
 
 
 
@@ -125,6 +195,15 @@ const activeLinks = ()=>{
 }
 
 
+
+const addCommasToNumber = (number)=>{
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')
+}
+
+
+//   Display Backdrop to the Details Pages
+
+
 // Init application
 const init = ()=>{
     switch(global.currentPage){
@@ -136,7 +215,7 @@ const init = ()=>{
             displayTvShows();
             break;
         case '/movie-details.html':
-            console.log('Movie details')
+            displayMovieDetails();
             break;
         case '/tv-details.html':
              console.log('Tv shows detail')
